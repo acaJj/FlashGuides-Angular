6a2714c4b0e80e06d6f7fdf7bb0ca5ff94c9adb4
Jeffrey Jaca <jeffreyjjaca@gmail.com>
2019-05-03 15:00:47 -0400

    fix dependency issue with tar package
    - update tar to 4.4.2 to fix Arbitrary File Overwrite issue
    - update firebase to latest version
    - run npm audit fix to fix 12 vulnerabilities

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+
+  firebaseConfig: {
+  	apiKey: "AIzaSyCJFLLSE3XKI_PkEG40F7pT4Qvh4DioAmM",
+    authDomain: "flash-guides.firebaseahpp.com",
+    databaseURL: "https://flash-guides.firebaseio.com",
+    projectId: "flash-guides",
+    storageBucket: "flash-guides.appspot.com",
+    messagingSenderId: "324807363723",
+  }
 };
